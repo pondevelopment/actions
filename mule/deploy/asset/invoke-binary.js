e0deb8a52827d9272b2214cf4800800e6de5c0ce
georgeroumieh-pon <george.roumieh@PON>
2024-07-01 14:23:30 +0200

    update the binary

--- mule/deploy/asset/invoke-binary.js
+++ mule/deploy/asset/invoke-binary.js
@@ -2,3 +2,5 @@ var childProcess = require("child_process");
 const binary = `mule-asset-deployment`
 const mainScript = `${__dirname}/${binary}`
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
+
+
