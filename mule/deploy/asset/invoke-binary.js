6fa1565ccee6c57e643b57fd69dbffeeaaa0c7ec
Christina Fayad <christina.fayad@pon.com>
2024-08-02 14:29:11 +0200

    Update mule-asset-deployment

--- mule/deploy/asset/invoke-binary.js
+++ mule/deploy/asset/invoke-binary.js
@@ -2,5 +2,3 @@ var childProcess = require("child_process");
 const binary = `mule-asset-deployment`
 const mainScript = `${__dirname}/${binary}`
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
-
-
