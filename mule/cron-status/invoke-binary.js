4966c5d49a58bd096dd2d2b994c6541b798c9f0d
Christina Fayad <christina.fayad@pon.com>
2024-10-11 13:21:50 +0200

    Update the js invoke code

--- mule/cron-status/invoke-binary.js
+++ mule/cron-status/invoke-binary.js
@@ -1,4 +1,17 @@
 var childProcess = require("child_process");
+const core = require('@actions/core');
+const exec = require('@actions/exec')
+core.addPath(`${__dirname}/terraform`);
+core.debug(`Installing CDKTF`);
+install();
 const binary = `mule-cron-status`
 const mainScript = `${__dirname}/${binary}`
-const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
+const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: ['inherit', 'inherit', 'pipe'] })
+console.log(spawnSyncReturns.status)
+if ( spawnSyncReturns.status == 1){
+    process.exit(1)
+}
+
+async function install() {
+    await exec.exec(`npm install -g cdktf-cli@0.15.0`);
+}
