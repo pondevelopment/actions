eb4d789955854212e3cf1911b4700f72f5f4e92f
Christina Fayad <christina.fayad@pon.com>
2024-10-16 16:21:55 +0200

    Reset the invoke-binary

--- mule/secure-properties/invoke-binary.js
+++ mule/secure-properties/invoke-binary.js
@@ -1,17 +1,21 @@
+// var childProcess = require("child_process");
+// const core = require('@actions/core');
+// const exec = require('@actions/exec')
+// core.addPath(`${__dirname}/terraform`);
+// core.debug(`Installing CDKTF`);
+// install();
+// const binary = `mule-secure-properties`
+// const mainScript = `${__dirname}/${binary}`
+// const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: ['inherit', 'inherit', 'pipe'] })
+// console.log(spawnSyncReturns.status)
+// if ( spawnSyncReturns.status == 1){
+//     process.exit(1)
+// }
+
+// async function install() {
+//     await exec.exec(`npm install -g cdktf-cli@0.15.0`);
+// }
 var childProcess = require("child_process");
-const core = require('@actions/core');
-const exec = require('@actions/exec')
-core.addPath(`${__dirname}/terraform`);
-core.debug(`Installing CDKTF`);
-install();
 const binary = `mule-secure-properties`
 const mainScript = `${__dirname}/${binary}`
-const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: ['inherit', 'inherit', 'pipe'] })
-console.log(spawnSyncReturns.status)
-if ( spawnSyncReturns.status == 1){
-    process.exit(1)
-}
-
-async function install() {
-    await exec.exec(`npm install -g cdktf-cli@0.15.0`);
-}
+const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
